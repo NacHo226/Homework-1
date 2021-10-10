a6711552d613d2680d83332d5161b56b4aeed5c8
NacHo226 <83178863+NacHo226@users.noreply.github.com>
2021-10-10 16:33:33 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = -455;
+            double d = 1088.9022;
+            float f = -1549.39f;
+            char c = '%';
+            short sh = -28;
+            bool b = true;
+            const decimal m = 333897987.3949M;
+            long l1 = 20003909;
+            var pi = 3.1415; // pi - типа double
+            string s1 = "Hello ", s2 = "world!", s3 = s1 + s2;
+            Console.WriteLine(s3);
+        }
+    }
+}
---
+++ Program2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 3;
+            Console.WriteLine(i++);          // output: 3
+            double a = 1.5;
+            Console.WriteLine(++a);          // output: 2.5
+            Console.WriteLine(i--);          // output: 4 
+            Console.WriteLine(-a);           // output: -2.5
+            Console.WriteLine(i * a);        // output: 7.5
+            Console.WriteLine(i / a);        // output: 1.2
+            Console.WriteLine(5 % 4);        // output: 1
+            Console.WriteLine(5 + 4);        // output: 9
+            Console.WriteLine(5 - 4);        // output: 1
+            Console.WriteLine(7.0 < 5.1);    // output: False
+            Console.WriteLine(7.0 <= 5.1);   // output: False
+            Console.WriteLine(7.0 >= 5.1);   // output: True
+            Console.WriteLine(!true);        // output: False
+            Console.WriteLine(true & false); // output: False
+            Console.WriteLine(true ^ true);  // output: False
+            Console.WriteLine(true | true);  // output: True
+            Console.WriteLine(true || true); // output: True
+            Console.WriteLine(true && false);// output: False
+            string s1 = "hello!";
+            string s2 = "hello!";
+            Console.WriteLine(s1 == s2);     // output: True
+            Console.WriteLine(s1 != s2);     // output: False
+        }
+    }
+}
